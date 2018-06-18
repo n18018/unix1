e8a0e9526d496b10c2afeddf6bdd81b9f603b305
n18018 <n18018@std.it-college.ac.jp>
2018-06-18 04:38:02 +0000

    6/14課題

--- 0614/061401.sh
+++ 0614/061401.sh
@@ -1,9 +1,6 @@
 #!/bin/bash 
 
 year=$1
-mod4=$(($year%4))
-mod100=$((year%100))
-mod400=$((year%400))
 
 if [ "$((year % 4))" -eq 0 -a "$((year % 100))" -ne 0 -o "$((year % 400))" -eq 0 ]; then
 	echo "$year"年はうるう年
